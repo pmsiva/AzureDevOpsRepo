28ab096c29ea8215f06487dba2230ba806c1ef3a
Siva Mahalingam <pmsiva@gmail.com>
2019-05-10 18:49:39 +1000

    Initial creation.

---
+++ App2.Backend/Controllers/ToDoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace App2.Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        [HttpGet("Items")]
+        public IActionResult GetTodoItems()
+        {
+            var todoList = new List<ToDoItem>()
+            {
+                new ToDoItem { Id = 1, Name = "Item 1" }
+            };
+
+            return Ok(todoList);
+        }
+    }
+}
---
+++ App2.Backend/ToDoItem.cs
@@ -0,0 +1,8 @@
+﻿namespace App2.Backend
+{
+    public class ToDoItem
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }        
+    }
+}
