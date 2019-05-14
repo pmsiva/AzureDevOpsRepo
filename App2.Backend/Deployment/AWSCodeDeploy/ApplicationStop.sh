b4f09fcf7736eceba12cebfc1b9ab32a2557e7fe
Siva Mahalingam <pmsiva@gmail.com>
2019-05-14 13:06:20 +1000

    test 9

--- App2.Backend/Deployment/AWSCodeDeploy/ApplicationStop.sh
+++ App2.Backend/Deployment/AWSCodeDeploy/ApplicationStop.sh
@@ -3,10 +3,5 @@
 #systemctl is-active --quiet kestrel-App2.service && systemctl stop kestrel-App2.service
 #systemctl is-enabled --quiet kestrel-App2.service && systemctl disable kestrel-App2.service
 
-#systemctl stop kestrel-App2.service
+systemctl stop kestrel-App2.service
 
-command="systemctl is-active --quiet kestrel-App2.service && systemctl stop kestrel-App2.service"
-eval " $command"
-
-command="systemctl is-enabled --quiet kestrel-App2.service && systemctl disable kestrel-App2.service"
-eval " $command"
